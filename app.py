7debbd8428a460627e1c92a7b8640d647b6965f8
rajputshivam <kumarshivam862@gmail.com>
2023-10-02 17:42:21 +0530

    data ingestion done

---
+++ app.py
@@ -0,0 +1,14 @@
+import sys
+
+from src.logger import logging
+from src.exception import CustomException
+
+if __name__ == "__main__":
+    logging.info("The execution has started")
+
+    try:
+        a = 1/0
+
+    except Exception as e:
+        logging.info("Custom Exception")
+        raise CustomException(e,sys)
